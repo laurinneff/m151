ddca3aa1b2d7ede8fdceabaaf83c268c06d76bca
lane <laurin@laurinneff.ch>
2022-12-15 14:21:35 +0100

    Make base controller more strict about what's a valid handler

--- thema1/src/BaseController.php
+++ thema1/src/BaseController.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use App\Attribute\Route as RouteAttribute;
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
 
 abstract class BaseController
 {
@@ -22,6 +24,20 @@ abstract class BaseController
 
             if (!$method->isStatic()) {
                 throw new \Exception('Route handler must be static');
+            }
+
+            if (!$method->isPublic()) {
+                throw new \Exception('Route handler must be public');
+            }
+
+            $params = $method->getParameters();
+            if (count($params) !== 1 || $params[0]->getType() === null || $params[0]->getType()->getName() !== ServerRequest::class) {
+                throw new \Exception('Route handler must accept a ServerRequest as its only parameter');
+            }
+
+            $returnType = $method->getReturnType();
+            if ($returnType === null || $returnType->getName() !== Response::class) {
+                throw new \Exception('Route handler must return a Response');
             }
 
             $route = $attributes[0]->newInstance();
