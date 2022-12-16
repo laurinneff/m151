7a9dc7e617a9c573d35ef7dd9fab1362ccfd69ca
lane <laurin@laurinneff.ch>
2022-12-16 11:42:50 +0100

    fix create db sql

--- DB/create.sql
+++ DB/create.sql
@@ -5,23 +5,23 @@ DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  id CHAR(36) NOT NULL,
+  id CHAR(36) NOT NULL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   pwd_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE accounts (
-  id CHAR(36) NOT NULL,
-  user_id CHAR(36) NOT NULL,
+  id CHAR(36) NOT NULL PRIMARY KEY,
+  user_id CHAR(36) NOT NULL REFERENCES users(id),
   balance DECIMAL(10, 2) UNSIGNED NOT NULL,
   name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE transactions (
-  id CHAR(36) NOT NULL,
-  account_from CHAR(36) NOT NULL,
-  account_to CHAR(36) NOT NULL,
+  id CHAR(36) NOT NULL PRIMARY KEY,
+  account_from CHAR(36) NOT NULL REFERENCES accounts(id),
+  account_to CHAR(36) NOT NULL REFERENCES accounts(id),
   amount DECIMAL(10, 2) NOT NULL,
   description VARCHAR(255) NOT NULL,
   timestamp DATETIME NOT NULL
